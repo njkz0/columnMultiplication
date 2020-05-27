031de4337eeef745ee65e5c1ff9fd34cdc1fe1d7
Anatoly <a_gavrik@bk.ru>
2020-05-27 19:42:11 +0300

    Initial commit

---
+++ src/info/sjd/WorkWithString.java
@@ -0,0 +1,12 @@
+package info.sjd;
+
+public class WorkWithString {
+    public static void main(String[] args) {
+        /* отличия билдер от буффер string
+        String.format()
+        regex регулярные выражения
+        внешние инсрументы работы со строкой apache StringUtills
+        срочные спецсцимволы или экранирование
+         */
+    }
+}
